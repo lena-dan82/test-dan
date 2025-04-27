2a35cb29fc7d179279cf604fd9863d4337cf456d
Danilov Aleksei <iirannok@gmail.com>
2025-04-27 18:33:56 +0300

    dz 1
    как преподали https://university.zerocoder.ru/pl/teach/control/lesson/view?id=336393143&editMode=0

---
+++ dz03.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+params = {
+    'userId' : 1
+}
+
+response = requests.get(url, params=params)
+
+if response.status.code == 200:
+    posts = response.json()
+    for post in posts:
+        print(post)
+else:
+    print("Error")
